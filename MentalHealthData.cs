9ecfb68742901c6635d984e7294ea7e6827ed261
masaki <40445257+masaki9@users.noreply.github.com>
2025-04-18 15:58:07 -0700

    Add init ML classifier

---
+++ MentalHealthData.cs
@@ -0,0 +1,26 @@
+
+
+using Microsoft.ML.Data;
+
+namespace MentalHealthSentimentAnalysisAPI;
+
+public class MentalHealthData
+{
+    [LoadColumn(0)]
+    public int Id { get; set; }
+
+    [LoadColumn(1)]
+    public string? Statement { get; set; }
+
+    [LoadColumn(2), ColumnName("Label")]
+    public string? Status { get; set; }
+}
+
+public class MentalHealthPrediction
+{
+    [ColumnName("PredictedLabel")]
+    public string? Prediction { get; set; }
+
+    [ColumnName("Score")]
+    public float[]? Score { get; set; } // Array of scores for each class
+}
